5eeca401f75b7cefdb888753e5ea9e93b621fdfa
emildamirov0 <emil.damirov.ed@gmail.com>
2022-03-04 10:42:36 +0100

    stats file

---
+++ stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  4 09:51:40 2022
+
+@author: emildamirov
+"""
+
+import linchackathon as lh
+import pandas as pd
+import seaborn as sns
+
+lh.getStockHistory('all', 30)
+data = pd.DataFrame(lh.getStockHistory('all', 30))
+
+data['Close'] = (data['bidClose'] + data['askClose'])/2
+stocks = data.pivot('time','symbol','Close')
+
+#%%
+
+stocks_returns = stocks.pct_change()
+
+stocks_mean_returns = stocks_returns.mean()
+stocks_mean_returns= stocks_mean_returns.sort_values(ascending=False)
+
+stocks_std_returns = stocks_returns.std()
+stocks_std_returns = stocks_std_returns.sort_values(ascending=False)
+
+stocks_sharpe = stocks_mean_returns/stocks_std_returns
+stocks_sharpe = stocks_sharpe.sort_values(ascending=False)
+
+
+#%%
